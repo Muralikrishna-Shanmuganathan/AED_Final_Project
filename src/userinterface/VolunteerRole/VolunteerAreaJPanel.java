f5182341a57f04f24b657117a7ed83071ac4df16
Muralikrishna Shanmuganathan <murali@Muralis-MacBook-Air.local>
2021-11-27 14:06:46 -0500

    Adding Volunteer and Store Manager code

--- src/userinterface/VolunteerRole/VolunteerAreaJPanel.java
+++ src/userinterface/VolunteerRole/VolunteerAreaJPanel.java
@@ -7,6 +7,7 @@ package userinterface.VolunteerRole;
 
 import Business.EcoSystem;
 import Business.UserAccount.UserAccount;
+import java.awt.CardLayout;
 import javax.swing.JPanel;
 
 /**
@@ -19,14 +20,14 @@ public class VolunteerAreaJPanel extends javax.swing.JPanel {
      * Creates new form VoluteerAreaJPanel
      */
     private JPanel userProcessContainer;
-    private EcoSystem business;
+    private EcoSystem ecosystem;
     private UserAccount userAccount;
 
-    public VolunteerAreaJPanel(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
+    public VolunteerAreaJPanel(JPanel userProcessContainer, UserAccount account, EcoSystem ecosystem) {
         initComponents();
         this.userProcessContainer = userProcessContainer;
         this.userAccount = account;
-        this.business = business;
+        this.ecosystem = ecosystem;
 
     }
 
@@ -40,29 +41,100 @@ public class VolunteerAreaJPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        btnAppRejStoreContribution = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         jLabel1.setText("Welcome Volunteer!");
+
+        btnAppRejStoreContribution.setText("Approve/Reject Store Contribution");
+        btnAppRejStoreContribution.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAppRejStoreContributionActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Approve/Reject Individual Contribution");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("View Profile");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(106, 106, 106)
+                .addGap(213, 213, 213)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(btnAppRejStoreContribution)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addContainerGap(219, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(170, Short.MAX_VALUE))
+                .addGap(135, 135, 135)
+                .addComponent(jButton4)
+                .addGap(26, 26, 26))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jLabel1)
-                .addContainerGap(250, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel1)
+                        .addGap(110, 110, 110)
+                        .addComponent(btnAppRejStoreContribution)
+                        .addGap(30, 30, 30)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(jButton4)))
+                .addContainerGap(135, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAppRejStoreContributionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAppRejStoreContributionActionPerformed
+        // TODO add your handling code here:
+        ApproveRejectStoreContributionJPanel arscjp = new ApproveRejectStoreContributionJPanel(userProcessContainer, ecosystem);
+        userProcessContainer.add("Manage Store Contribution", arscjp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_btnAppRejStoreContributionActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        ApproveRejectIndContributionJPanel aricjp = new ApproveRejectIndContributionJPanel(userProcessContainer, ecosystem);
+        userProcessContainer.add("Manage Individual Contribution", aricjp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        ViewVolunteerProfileJPanel vvpjp = new ViewVolunteerProfileJPanel(userProcessContainer, ecosystem);
+        userProcessContainer.add("View Profile", vvpjp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAppRejStoreContribution;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
