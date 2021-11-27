16b6d372f39f1693c336fc8c63a1d13297d2c950
Muralikrishna Shanmuganathan <murali@Muralis-MacBook-Air.local>
2021-11-27 02:19:17 -0500

    adding WorkRequest & role code

--- src/Business/WorkQueue/WorkRequest.java
+++ src/Business/WorkQueue/WorkRequest.java
@@ -8,6 +8,7 @@ package Business.WorkQueue;
 import Business.DeliveryAgency.Clerk;
 import Business.DeliveryAgency.Driver;
 import java.util.Date;
+import Business.UserAccount.UserAccount;
 
 /**
  *
@@ -20,18 +21,68 @@ public class WorkRequest {
     int workRequestID=(int)(Math.random()*1000)+1;
     //check for repetetion of workRequestID
     
-    String volunteerName;
-    String DeliveryAgencyName;
-    String Status;
-    String pickUpLocation;
-    String dropOffLocation;
-    Date pickUptime;
-    Date dropOfftime;
-    String VehicleNumber;  
-    Driver driver;
-    Clerk clerk;
+    private String volunteerName;
+    private String DeliveryAgencyName;
+    private String Status;
+    private String pickUpLocation;
+    private String dropOffLocation;
+    private Date pickUptime;
+    private Date dropOfftime;
+    private String VehicleNumber;  
+    private Driver driver;
+    private Clerk clerk;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserAccount receiver) {
+        this.receiver = receiver;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+    
+    private String message;
+    private UserAccount sender;
+    private UserAccount receiver;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    
+    
    
     public WorkRequest() {
+        requestDate = new Date();
     }
 
     public int getWorkRequestID() {
